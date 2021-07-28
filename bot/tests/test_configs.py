624d82ad9003b0998ef32792b770c3b78e5c9104
Cristian Hainic <cristian.hainic@evozon.com>
2021-07-28 10:10:56 +0300

    fix test

--- bot/tests/test_configs.py
+++ bot/tests/test_configs.py
@@ -1,9 +1,11 @@
+import os
 import urllib.parse
 
 from bot.configs import api_key, webhook
 
 
 async def test_api_key():
+    os.system('printf "123asd-mybotkey-blabla" > /run/secrets/BOT_API_KEY')
     key = await api_key()
     assert isinstance(key, str), not key.isspace()
 
